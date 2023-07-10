c300500987a3b78028e36324a17cb1082a20ca02
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2023-07-11 01:07:17 +0530

    Uploaded the project after building it in local

---
+++ CompressDecompress/src/main/java/com/mycompany/compressdecompress/CompressDecompress.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compressdecompress;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class CompressDecompress {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
